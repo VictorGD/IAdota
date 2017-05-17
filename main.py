ba00063f6086b02cc8987c2418ba3244b5daf765
VictorGD <victor.godier@gmail.com>
2017-05-17 16:48:18 +0200

    commit

--- main.py
+++ main.py
@@ -1,90 +1,133 @@
 import csv
 
+#HEROES LIST
+heroes = []
+with open("dota2heroes.csv", 'r') as f:
+    reader = csv.reader(f)
+    r = list(reader)
+    for row in r :
+        heroes.append(row[0])
+
+#GAME TO PREDICT
+game = []
+with open("dota2game.csv", 'r') as f:
+    reader = csv.reader(f, delimiter=';')
+    r = list(reader)
+    rownum = 0
+    for row in r :
+        if rownum ==1 :
+            for elem in row:
+                game.append(elem)
+        rownum+=1
+
+#MATCH HISTORY
 with open("dota2.csv", 'r') as f:
     reader = csv.reader(f, delimiter=';')
     r = list(reader)
-
-    # INITIALISATION
-    rownum =0
     game_history = []
     for row in r :
-        if rownum == 0 :
-            heroes = row # Heroes Table
-        elif rownum == 1 :
-            game = row # The table to compare
-        else :
-            game_history.append(row) # The match history table
-        rownum+=1
-    del game[0]
-    del heroes[0]
+        game_history.append(row)
+for row in game_history:
+    print(row)
 
-    stats_game = []
-
-    # GAME SCORING
-    score_max = 0
-    rownum = 0
-    for row in game_history :
-        row.append(0)
-        row.append(0)
-
-        indice_hero = 0
-        for hero in game :
-            if hero != '0' :
-                if row[indice_hero+1] == hero :
-                    row[len(row)-2]+=1
-            indice_hero += 1
-
+stats_game = []
+# GAME SCORING
+score_max = 0
+rownum = 0
+for row in game_history :
+    row.append(0)
+    row.append(0)
+    indice_hero = 0
+    for hero in game :
+        if hero != '0' :
+            if row[indice_hero+1] == hero :
+                row[len(row)-2]+=1
+        indice_hero += 1
         indice_hero =0
-        for hero in game :
-            if hero != '0' :
-                if row[indice_hero+1] == str(- int(hero)) :
-                    row[len(row)-1]+=1
-            indice_hero += 1
-
-        if row[len(row)-2] >= score_max :
-            score_max = row[len(row)-2]
-            stats_game.append([row[0],row[len(row)-2],rownum])
-        if row[len(row)-1] >= score_max :
-            score_max = row[len(row)-1]
-            stats_game.append([- int(row[0]),row[len(row)-1],rownum])
-
+    for hero in game :
+        if hero != '0' :
+            if row[indice_hero+1] == str(- int(hero)) :
+                row[len(row)-1]+=1
+        indice_hero += 1
+    if row[len(row)-2] >= score_max :
+        score_max = row[len(row)-2]
+        stats_game.append([row[0],row[len(row)-2],rownum])
+    if row[len(row)-1] >= score_max :
+        score_max = row[len(row)-1]
+        stats_game.append([- int(row[0]),row[len(row)-1],rownum])
         rownum +=1
-
     # CLEANING STATS
-    rownum =0
-    for row in stats_game :
-        if row[1]<score_max :
-            del stats_game[rownum]
-        rownum+=1
-
-    #PREDICTION
-    victories = 0
-    defeats = 0
-    for row in stats_game :
-        if row[0]==1:
+rownum =0
+for row in stats_game :
+    if row[1]<score_max & len(stats_game)>(len(game_history)/1000) :
+        del stats_game[rownum]
+    rownum+=1
+#PREDICTION
+victories = 0
+defeats = 0
+for row in stats_game :
+    if row[0]==1:
+        if row[1]==score_max:
+            victories+=512
+        if row[1]==score_max-1:
+            victories+=256
+        if row[1]==score_max-2:
+            victories+=128
+        if row[1]==score_max-3:
+            victories+=64
+        if row[1]==score_max-4:
+            victories+=32
+        if row[1]==score_max-5:
+            victories+=16
+        if row[1]==score_max-6:
+            victories+=8
+        if row[1]==score_max-7:
+            victories+=4
+        if row[1]==score_max-8:
+            victories+=2
+        if row[1]==score_max-9:
             victories+=1
-        else :
-            defeats+=1
-    prediction = (victories-defeats)/(victories+defeats)*100
-
-    #PRINT PREDICTION
-    if prediction > 0 :
-        print ('Victory '+str(prediction)+'%')
-    elif prediction < 0 :
-        print ('Defeat '+str(prediction+100)+'%')
     else :
-        print ('Draw')
-
-    # print ('############# HEROES ###########')
-    # for row in heroes :
-    #     print(row)
-    # print ('\n################# GAME ###############')
-    # for row in game :
-    #     print(row)
-    # print ('\n################# GAME_HISTORY ##################')
-    # for row in game_history :
-    #     print(row)
-
-    # print (r[1][0])
-    # for row in reader:
-    #     print(row)
+        if row[1]==score_max:
+            defeats+=512
+        if row[1]==score_max-1:
+            defeats+=256
+        if row[1]==score_max-2:
+            defeats+=128
+        if row[1]==score_max-3:
+            defeats+=64
+        if row[1]==score_max-4:
+            defeats+=32
+        if row[1]==score_max-5:
+            defeats+=16
+        if row[1]==score_max-6:
+            defeats+=8
+        if row[1]==score_max-7:
+            defeats+=4
+        if row[1]==score_max-8:
+            defeats+=2
+        if row[1]==score_max-9:
+            defeats+=1
+prediction = (victories-defeats)/(victories+defeats)*100
+#PRINT PREDICTION
+radiant_team = []
+dire_team = []
+colnum = 0
+for hero in game :
+    if hero == '1' :
+        radiant_team.append(heroes[colnum])
+    if hero == '-1' :
+        dire_team.append(heroes[colnum])
+    colnum+=1
+    print('Radiant Team :')
+for hero in radiant_team :
+    print('  - '+str(hero))
+print('Dire Team :')
+for hero in dire_team :
+    print('  - '+str(hero))
+    if prediction > 0 :
+    print ('Dire Victory '+str(int(prediction))+'%')
+elif prediction < 0 :
+    print ('Radiant Victory '+str(int(prediction+100))+'%')
+else :
+    print ('Draw')
